59afc01f77973e4ad1c827780d7223b65ea0898d
Mubara-Fatima <mubarafatima58@gmail.com>
2026-07-27 14:31:25 +0500

    Update server.py

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ def emot_detector():
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
     
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
+    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
